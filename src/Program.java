eacf2c3e8470801e6fa70c8f36c562fce3065d99
YUNUS <yunus@YUNUSs-MacBook-Pro.local>
2022-08-17 13:38:23 +0200

    first commit

---
+++ src/Program.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		
+		/*
+		String [] employees = new String [4];
+		
+		employees[0] = "Xidir";
+		employees[1] = "Bayar";
+		employees[2] = "Blazer";
+		employees[3] = "Waisi";
+		
+		String [] employeesNew = new String [8];
+		
+		for( int i = 0 ; i < employees.length; i++) {
+			employeesNew[i]= employees[i];
+			
+	}
+		employees = employeesNew;
+	*/	
+	
+	ArrayList <String> employees = new ArrayList <String>();
+	
+	employees.add("Xidir");
+	employees.add("Bayar");
+	employees.add("Blazer");
+	employees.add("Waisi");
+	
+										// Es geht noch weiter aber sind noch zu machen 
+	}
+}    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
